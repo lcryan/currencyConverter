c298498a263a2b49dc625737c79f70db2d0f11b4
LuciaRyan <lcryan06@gmail.com>
2023-05-10 11:59:09 +0200

    started building a currency converter with a scanner for the user's input - still in progress. Variables are defined for: conversion rate, euro, dollar. Later will add other currencies as well.

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,16 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-// this should be the user's input within the scanner,
-// the user is supposed to only get the user input and then the machine calculates
-// the currency exchange- how to solve this with the scanner ?
-        double euro = 5.8;
-        double dollar = 13.5;
-        double yuan = 350;
+
+//1. I need the conversion rate. Then I have to make two variables for Euro and Dollar.  - DONE.
+        //2.
+        double conversionDollarToEuro = 1.19;
+
+        double euro;
+        double dollar;
 
 
-        double currencyRateEuroToDollar = euro * 1.10;
-        double currencyDollarRateToEuro = dollar * 0.91;
-        double currencyYuanToEuro = yuan * 0.13;
-
-/*        Scanner myObj = new Scanner(System.in);
-        System.out.println("Enter currency");
-        currencyDollarRateToEuro = Double.parseDouble(myObj.nextLine());
-
-        System.out.println("Your currency exchange is: " + currencyDollarRateToEuro);*/
-
-        System.out.println(euro + " euro " + "are currently " + currencyRateEuroToDollar + " dollar!");
-        System.out.println(dollar + " dollar " + "are currently " + currencyDollarRateToEuro + " euro!");
-        System.out.println(yuan + " yuan " + "are currently " + currencyYuanToEuro + " euro!");
     }
 }
 
